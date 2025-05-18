a0765401b723eeac27230cf15d2e2d40dd7cc620
Knnthiti <sriparsonk14823@gmail.com>
2025-05-18 20:49:48 +0700

    Update espnow_ROBOT.cpp

--- src/espnow_ROBOT.cpp
+++ src/espnow_ROBOT.cpp
@@ -20,9 +20,6 @@ void OnDataSent(const uint8_t* mac_addr, esp_now_send_status_t status) {
 }
 
 void ESPNOW_ROBOT ::Setup_send_ESPNOW() {
-  // Set up Serial Monitor
-  Serial.begin(115200);
-
   WiFi.mode(WIFI_STA);
 
   // Initilize ESP-NOW
@@ -62,13 +59,9 @@ void ESPNOW_ROBOT ::Sendvalue_ESPNOW(uint8_t* data, size_t len) {
     Serial.println(" | Sending error");
 #endif
   }
-  delay(10);
 }
 
 void ESPNOW_ROBOT ::Setup_receive_ESPNOW() {
-  // Set up Serial Monitor
-  Serial.begin(115200);
-  
   // Set ESP32 as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
